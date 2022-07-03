633a23c86b51cd39f355d7f7490994c997ce4a17
Vitaliy-Zinevich <everwoods21011993@gmail.com>
2022-07-03 20:36:33 +0300

    React.lazy

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import React from "react";
 import './scss/app.scss';
 import Header from './components/Header';
 import Home from './pages/Home';
-import Cart from './pages/Cart';
-import NotFound from './pages/NotFound';
+// import Cart from './pages/Cart';
+// import NotFound from './pages/NotFound';
 import { Routes, Route } from 'react-router-dom';
 
+
+const Cart = React.lazy(() => import(/* webpackChunkName: "Cart" */ './pages/Cart'));
+const NotFound = React.lazy(() => import(/* webpackChunkName: "NotFound" */ './pages/NotFound'));
 
 function App() {
   return (
@@ -13,8 +17,20 @@ function App() {
       <div className="content">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="*" element={<NotFound />} />
+          <Route 
+           path="/cart" 
+           element={
+           <React.Suspense fallback={<div>Загрузка...</div>}>
+             <Cart />
+           </React.Suspense>} 
+           />
+          <Route
+           path="*" 
+           element={
+            <React.Suspense fallback={<div>Загрузка...</div>}>
+              <NotFound />
+            </React.Suspense>} 
+            />
         </Routes>
       </div>
     </div>
