b6f70be606c2883129874ea0c433817134752fbb
Vitaliy-Zinevich <everwoods21011993@gmail.com>
2022-07-05 19:27:20 +0300

    icon and titlee

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import React from "react";
 import './scss/app.scss';
 import Header from './components/Header';
 import Home from './pages/Home';
-// import Cart from './pages/Cart';
-// import NotFound from './pages/NotFound';
 import { Routes, Route } from 'react-router-dom';
 
 
